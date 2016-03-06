b8f93045514cae0766090fc18427051c4df7c6cd
Steve Cox <stevecox@Alpha-A.local>
2016-03-05 20:02:04 -0800

    corrected the effective dia of the wheel

--- Auto/Oak_9804_Auto_v1.java
+++ Auto/Oak_9804_Auto_v1.java
@@ -17,6 +17,7 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
     DcMotor driveLeftFront;
     DcMotor driveRightBack;
     DcMotor driveRightFront;
+    //two encoders are on rear motors, 2 are mounted on tread module
     DcMotor spin;
 
     //servos to lock in place on ramp
@@ -31,25 +32,23 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
 
     double midPower;
     int targetHeading;
-    double driveGain = 0.1;
+    double driveGain;
     double leftPower;
     double rightPower;
-    int currentHeading = 0;                     //This is a signed value
+    int currentHeading = 0;                     //This is a signed value, CCW is positive
     int headingError;
     double driveSteering;
     double currentDistance;
     int currentEncCountLeft;
     int currentEncCountRight;
 
-
-    double targetDistance;
-    int encoderCountsPerRotation = 1120;
-    double radius = 1.86; //using math is 1.86
-    double diameter = radius * 2;
-    double circumference = diameter * 3.14159;
+    
+    double targetDistance;                      //magnitude in distance
+    final int encoderCountsPerRotation = 1120;
+    final double diameter = 2.583;       //effective diameter of drive pulley and tread b/c of sinking into foam mat (measures 3" dia)
+    double circumference = diameter * 3.14159; //C=PI*D
     double rotations;
     int targetEncoderCounts;
-    int targetEncoderCounts1;
     int EncErrorLeft;
     int telemetryVariable;
     int initialEncCountLeft;
@@ -69,6 +68,10 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        //USE CONFIGURATION FILE 'JABBED' ON BOTH MAIN AND B PHONES
+
+
         //gives name of drive motors
         driveLeftBack = hardwareMap.dcMotor.get("m5");      // 1 on red controller SN VUTK
         driveLeftFront = hardwareMap.dcMotor.get("m6");     // 2 on red
@@ -96,17 +99,20 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
 
         gyro.calibrate();
 
+
+        //begin match with driver station button
         waitForStart();
 
         // make sure the gyro is calibrated.
         while (gyro.isCalibrating()) {
             Thread.sleep(50);
         }
+
         driveLeftBack.setDirection(DcMotor.Direction.FORWARD);
         driveLeftFront.setDirection(DcMotor.Direction.FORWARD);
         driveRightBack.setDirection(DcMotor.Direction.REVERSE);
         driveRightFront.setDirection(DcMotor.Direction.REVERSE);
-
+        //2 encoders are on rear motors, two are mounted in tread module
 
 
         //DRIVE BACKWARDS 60 INCHES
@@ -116,14 +122,15 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
         driveGain = 0.05;           //gain for proportional control
 
         midPower = 0.5;             //the midpower with which we add and subtract the drive steering from
-        targetHeading = 0;              //drive straight ahead at the initial heading
+        targetHeading = 0;              //drive straight ahead at the initial/default heading
 
         targetDistance = 60;          //drive straight 60 inches
 
         //math for target encoder counts to travel
         rotations = targetDistance / circumference;
         targetEncoderCounts = (int) (encoderCountsPerRotation * rotations);
-        targetEncoderCounts1 = targetEncoderCounts;
+
+        spin.setPower(0);
 
         //resets start time before starting the drive
         this.resetStartTime();
@@ -132,12 +139,19 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
         initialEncCountLeft = driveLeftBack.getCurrentPosition();
         initialEncCountRight = driveRightBack.getCurrentPosition();
 
+        //allow time to read encoder value
+        for (int i=0; i< 10; i++){
+            waitOneFullHardwareCycle();
+        }
+
 
         do {
             spin.setPower(1);  // Eject debris while driving, to clear path
 
             currentEncCountLeft = driveLeftBack.getCurrentPosition() - initialEncCountLeft;         //the current - initial will give the
             currentEncCountRight = driveRightBack.getCurrentPosition() - initialEncCountRight;      //current distance of the encoders
+
+            waitOneFullHardwareCycle();         //allow time for the hardware to execute its task
 
             EncErrorLeft = targetEncoderCounts - Math.abs(currentEncCountLeft);                     //the error is the delta between the target counts and current counts
 
@@ -184,7 +198,7 @@ public class Oak_9804_Auto_v1 extends LinearOpMode {
 
 
         } while (EncErrorLeft > 0                   //the error is slowly decreasing, so run while greater than 0
-                && this.getRuntime() < 30);         //safety timeout of 10 seconds
+                && this.getRuntime() < 100);         //safety timeout of 100 seconds
 
         //set all motor powers to 0 after drive code finished running
         driveLeftBack.setPower(0.0);
